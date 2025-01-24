465675b4af25bb80c60dd861039d1da2fad4bada
Banashree Aribenchi <banashree.aribenchi17@gmail.com>
2025-01-24 15:25:24 +0100

    Add header section

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import React from "react";
+import logo from "./src/images/Food_Delivery_App_Logo.png";
 import ReactDOM from "react-dom/client";
 
-const App = () => {
-  return <h1>Welcome</h1>;
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={logo} alt="Fressen Logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(<AppLayout />);
